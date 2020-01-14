d4ac274cb85cc7e70a4ebd1b656847e476350191
Infodensoft <Infodensoft2000@gmail.com>
2020-01-14 10:51:17 +0530

    Initial Commit

---
+++ TableViewWithMultipleCellTypes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewWithMultipleCellTypes
+//
+//  Created by lab2 on 24/10/1941 Saka.
+//  Copyright © 1941 lab2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
